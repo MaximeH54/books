2ad5fcd1c93eb64da737289c3fc86092e7a811e3
Maxime Hemmerlé <maxime_hemmerle@orange.fr>
2019-10-31 16:00:59 +0100

    models/views update

--- views/books.php
+++ views/books.php
@@ -1,6 +1,21 @@
 <?php $title = "Books Lists"; ?>
 <?php ob_start(); ?>
-<h1>Books lists<h1>
+<h1>Books lists</h1>
+
+<ul>
+  <?php
+    foreach ($books as $book) {
+      ?>
+      <li><?php echo $book['title']; ?></li>
+      <?php
+      //code...
+    }
+  ?>
+</ul>
+
+  <pre>
+    <?php var_dump($books); ?>
+  </pre>
   <p>Welcome in this section</p>
   <?php $content = ob_get_clean(); ?>
 
